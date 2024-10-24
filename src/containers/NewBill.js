66c7a53bcd9b98cedb11bb4b120488ab6531a565
Kushchina Alena <alenakushchina@gmail.com>
2024-10-24 21:09:48 +0200

    fixed the bug with displaying bills

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -20,6 +20,16 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
+
+    const allowedExtensions = ['jpg', 'jpeg', 'png'];
+    const fileExtension = fileName.split('.').pop().toLowerCase();
+
+    if (!allowedExtensions.includes(fileExtension)) {
+        alert("Seuls les fichiers jpg, jpeg ou png sont acceptés.");
+        e.target.value = "";
+        return;
+    }
+
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
